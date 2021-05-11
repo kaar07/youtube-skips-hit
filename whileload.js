026800952c9bf2ec9545f7fe5b5745f7c24bcb8d
kaar07 <kaarthik1415522@gmail.com>
2021-05-11 18:55:44 +0530

    intial commit

---
+++ whileload.js
@@ -0,0 +1,4 @@
+let oldURL = window.location.href;
+if(oldURL === 'https://www.youtube.com' || oldURL === 'https://www.youtube.com/'){
+    window.location.href = 'https://www.youtube.com/feed/subscriptions'
+}
